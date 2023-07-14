523bff869a8111729b7a8f4e83fc7686c6cee333
Regule <regule@runbox.com>
2023-07-14 20:43:25 +0200

    More work

--- Scripts/visualization.py
+++ Scripts/visualization.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import datetime
+import time
 import pygame
 import os
 from moviepy.editor import *
@@ -30,11 +32,71 @@ class HeadCanvas:
             screen.blit(gif_surface, self.position)
 
 
+class Position:
+
+    def __init__(self, timestamp, lat, lon):
+        self.lat = lat
+        self.lon = lon
+        self.timestamp = timestamp
+
+
+
+class Satellites:
+
+    def __init__(self, folder, sampling_rate):
+        self.sats = {}
+        for entry in os.scandir(folder):
+            if entry.is_file():
+                sat_name = entry.name.split('.')[0].replace('_',' ')
+                sat_path = os.path.join(folder, entry.name)
+                print(f'Satellite {sat_name} at path {sat_path}')
+                sat_file = open(sat_path, "r")
+                self.sats[sat_name] = sat_file 
+        self.update_period = MILLISECONDS_PER_SECOND/sampling_rate
+        self.time_since_last_update = 0
+        self.positions = {}
+
+    def update_positions(self):
+        for sat_name, sat_file in self.sats.items():
+            line = sat_file.readline()
+            timestamp, lat, lon = line.split(';')
+            timestamp = int(timestamp)
+            lat = float(lat)
+            lon = float(lon)
+            position = Position(timestamp, lat, lon)
+            self.positions[sat_name] = position
+
+    def set_initial_readout(self, initial_timestamp=None):
+        if initial_timestamp is None:
+            initial_timestamp = datetime.now()
+            initial_timestamp = time.mktime(initial_timestamp.timetuple())
+            self.update_positions()
+            while list(self.positions.values())[0].timestamp < initial_timestamp:
+                self.update_positions()
+
+
+    def update(self):
+        pass
+
+    def cleanup(self):
+        for _, file in self.sats:
+            file.close()
+
+class EarthCanvas:
+
+    def __init__(self, image_path, satellites, position, size):
+        self.backdrop = pygame.image.load(image_path)#.convert()
+        self.position = position
+        self.size = size
+        self.satellites = satellites
+
+    def cleanup(self):
+        self.satellites.cleanup()
+
 class ManWhoLaughsDisplay:
 
-    def __init__(self, head, satellites, earth, screen_size):
+    def __init__(self, head, earth, screen_size):
         self.head = head
-        self.satellistes = satellites
         self.earth = earth
         self.screen = pygame.display.set_mode(screen_size)
         self.clock = pygame.time.Clock()
@@ -50,24 +112,49 @@ class ManWhoLaughsDisplay:
             if event.type == pygame.QUIT:
                 raise StopIteration
 
+    def cleanup(self):
+        self.earth.cleanup()
+
+
+def integer_pair(txt):
+    val1, val2 = txt.split('x')
+    return (int(val1), int(val2))
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-g', '--gif_file', type=str, required=True,
                         help='Path to GIF file')
     parser.add_argument('--gif_fps', type=int, default=60,
                         help='Frames per second of gif animation')
+    parser.add_argument('--gif_position', type=integer_pair, default=(0,0),
+                        help='Position of top left corner of gif.')
+    parser.add_argument('--window_size', type=integer_pair, default=(800,600),
+                        help='Size of the window in pixels.')
+    parser.add_argument('-s', '--satellite_directory', type=str, required=True,
+                        help='Folder with satellite files')
+    parser.add_argument('-e', '--earth_file', type=str, required=True,
+                        help='Path to earth backdrop image')
+    parser.add_argument('--initial_timestamp', type=int, default=None,
+                        help='Initial timestampt in unix format, set only during tests.')
+    parser.add_argument('--sampling_rate', type=int, default=1,
+                        help='Sampling rate of satellite positions in seconds')
     return parser.parse_args()
 
 
 def main():
+    pygame.init()
     args = parse_arguments()
-    head = HeadCanvas(args.gif_file, args.gif_fps, (0,0))
-    mwl_display = ManWhoLaughsDisplay(head, None, None, (800,600))
+    satellites = Satellites(args.satellite_directory, args.sampling_rate)
+    satellites.set_initial_readout(args.initial_timestamp)
+    earth = EarthCanvas(args.earth_file, satellites, (0,0), (800, 600))
+    head = HeadCanvas(args.gif_file, args.gif_fps, args.gif_position)
+    mwl_display = ManWhoLaughsDisplay(head, earth, args.window_size)
     try:
         while True:
             mwl_display.update()
     except StopIteration:
         print('Stopped')
+        mwl_display.cleanup()
 
 if __name__ == '__main__':
     main()
