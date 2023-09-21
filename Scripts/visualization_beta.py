17d6dc329e958e162339a626d8b80132b04b0bed
Regule <regule@runbox.com>
2023-09-21 12:22:32 +0200

    Started work on beta

--- Scripts/visualization_beta.py
+++ Scripts/visualization_beta.py
@@ -6,6 +6,37 @@ import os
 import pygame as pg
 from datetime import datetime
 
+#===================================================================================================
+#                                             MODEL 
+#===================================================================================================
+
+def coordinates_to_authagraph(location):
+    # Constants for AuthaGraph projection
+    radius = 1.0  # Radius of the AuthaGraph sphere
+
+    # Convert latitude and longitude to radians
+    lat_rad = math.radians(location[0])
+    lon_rad = math.radians(location[1])
+
+    # Calculate AuthaGraph coordinates
+    x = radius * (1 + 0.5 * math.cos(lat_rad) * math.cos(lon_rad))
+    y = radius * (1 + 0.5 * math.cos(lat_rad) * math.sin(lon_rad))
+    #z = radius * 0.5 * math.sin(lat_rad)
+
+    return x, y#, z
+
+class Position:
+
+    def __init__(self, timestamp, lat, lon):
+        self.lat = lat
+        self.lon = lon
+        self.timestamp = timestamp
+
+    def get_coordinates(self):
+        return [self.lat, self.lon]
+
+    def __str__(self):
+        return f'{self.timestamp} - [{self.lat} {self.lon}]'
 
 #===================================================================================================
 #                                            DISPLAY
